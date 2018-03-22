b83a04b836a1349d314e11cdbcbf0ff230d253be
Paul Xia <xialaohu2008@163.com>
2018-03-22 16:13:20 +0800

    2018年3月22日 Psychology_Bot第一次commit（在大创基地）

---
+++ src/Server/Config/server.js
@@ -0,0 +1,14 @@
+const path = require("path")
+
+module.exports = {
+    https: false,
+    port: 81,
+    caFile: path.join( __dirname , "./ca-bundle.pem" ),
+    keyFile: path.join( __dirname , "./antinux_me.key" ),
+    certFile: path.join( __dirname , "./cert.pem" ),
+    session: {
+      secret: "Antinux",
+      resave: true,
+      saveUninitialized: true
+    }
+};
