bf7f403f1c3f4d551c2c9e5e20d1eb8eb7ad3a1b
GitAntinus <gantinus@gmail.com>
2018-03-31 21:24:46 +0800

    this is a quick commit

--- src/Frontend/Connected/ConvertAudioToText/actions.js
+++ src/Frontend/Connected/ConvertAudioToText/actions.js
@@ -1,7 +1,15 @@
 import {
 	__sendConvert
 } from "actionType";
+
 import md5 from "js-md5";
+
+import bufferToWav from "audiobuffer-to-wav";
+
+const audioCtx = new AudioContext();
+
+const reader = new FileReader();
+
 
 export const sendConvertStart = () => ({
 	type: __sendConvert.pending
@@ -27,79 +35,55 @@ export const sendConvert = ({ audio }) => dispatch => {
 		return;
 	}
 	dispatch( sendConvertStart() );
-	//------------------------------------------------urlencode
-	function str2asc(strstr){ 
-		return ("0"+strstr.charCodeAt(0).toString(16)).slice(-2); 
-		} 
-	function asc2str(ascasc){ 
-		return String.fromCharCode(ascasc); 
-	} 
-	function UrlEncode(str){      
-   			var ret="";      
-   			var strSpecial="!\"#$%&'()*+,/:;<=>?[]^`{|}~%";      
-   			var tt= "";     
- 
-   		for(var i=0;i<str.length;i++){      
-   		var chr = str.charAt(i);      
-    	 var c=str2asc(chr);      
-    	tt += chr+":"+c+"n";      
-    	if(parseInt("0x"+c) > 0x7f){      
-      	ret+="%"+c.slice(0,2)+"%"+c.slice(-2);      
-    	}else{      
-      if(chr==" ")      
-        ret+="+";      
-      else if(strSpecial.indexOf(chr)!=-1)      
-        ret+="%"+c.toString(16);      
-      else      
-        ret+=chr;      
-    }      
-   }      
-  	return ret;      
-  	}
-	//----------------------------------------------------
-	const reader = new FileReader();
-	reader.readAsDataURL( audio );
-	reader.onloadend = () => {
-		const apiId = '5ab67bf9';
-		const apiKey = "b60768780fde86f7b55c8bdbffc2a53b";
-		const currentTime = Date.now();
-		const ParamJson = {
-			"engine_type":"sms16k",
-			 "aue": "raw"
-		};
- 
- 
-		const XParam = btoa( JSON.stringify(ParamJson) );
-		const XCheckSum = md5( apiKey + currentTime + XParam );
- 
-		console.log( audio , reader.result.slice( reader.result.indexOf(',') + 1, reader.result.length ) );
-		var resultURL=UrlEncode(reader.result.slice( reader.result.indexOf(',') + 1, reader.result.length ));
- 
-		fetch( "/api/AudioToText" , {
-			method: "POST",
-			headers: {
-				"Content-Type": "x-www-form-urlencoded; charset=utf-8",
-				"X-Appid": apiId,
-				"X-CurTime": currentTime,
-				"X-Param": XParam,
-				"X-CheckSum": XCheckSum
-			},
-			body: resultURL
-		}).then( response => {
-			if( !response.ok ){
-				throw "network"
-			} else {
-				response.json().then( json => {
-					console.log( json );
-					if( json.code !== "0" ){
-						dispatch( sendConvertFailed({ reason: "server" }) );
-					} else {
-						dispatch( recieveText({ text: json.data }) );
-					}
-				});
-			}
-		}).catch( err => {
-			dispatch( sendConvertFailed({ reason: err }) );
-		})
-	}
+
+	const apiId = '5ab67bf9';
+	const apiKey = "b60768780fde86f7b55c8bdbffc2a53b";
+	const currentTime = Math.floor( Date.now() / 1000 );
+	const ParamJson = {
+		"engine_type":"sms16k",
+		 "aue": "raw"
+	};
+
+
+	const XParam = btoa( JSON.stringify(ParamJson) );
+	const XCheckSum = md5( apiKey + currentTime + XParam );
+
+  reader.readAsArrayBuffer( audio );
+
+  reader.onloadend = () => {
+    const arrayBuffer = reader.result;
+    audioCtx.decodeAudioData( arrayBuffer  , buffer => {
+      let wav = bufferToWav( buffer );
+      let blob = new Blob( [new DataView( wav )] , {
+        type: "audio/wav"
+      });
+	    fetch( "/api/AudioToText" , {
+	  	  method: "POST",
+	       headers: {
+           "Content-Type": "x-www-form-urlencoded; charset=utf-8",
+	  		   "X-Appid": apiId,
+	  		   "X-CurTime": currentTime,
+	  		   "X-Param": XParam,
+	  		   "X-CheckSum": XCheckSum
+	  	   },
+	  	   body: `audio=${btoa( blob )}`
+	     }).then( response => {
+	  	   if( !response.ok ){
+	  		   throw "network"
+	  	   } else {
+	  		   response.json()
+           .then( json => {
+	  			 console.log( json );
+	  			 if( json.code !== "0" ){
+	  				 dispatch( sendConvertFailed({ reason: "server" }) );
+	  			 } else {
+	  			   dispatch( recieveText({ text: json.data }) );
+           }
+	  		 });
+	  	 }
+	  }).catch( err => {
+	  	dispatch( sendConvertFailed({ reason: err }) );
+	  })
+    })
+  }
 }
--- src/Server/API/ConvertAudioToText.js
+++ src/Server/API/ConvertAudioToText.js
@@ -1,22 +1,20 @@
 const fetch = require("node-fetch");
 
-
 module.exports = ({ res , req }) => {
-	
-	
-	fetch( "http://api.xfyun.cn/v1/service/v1/iat" , {
-		method: "POST",
-		headers: {
-			"Content-Type":req.get("Content-Type"),
-			"X-Appid":req.get("X-Appid"),
-			"X-CurTime":req.get("X-CurTime"),
-			"X-Param":req.get("X-Param"),
-			"X-CheckSum":req.get("X-CheckSum")
-		},
-		body: req.body
-	}).then( response => {
-		response.json().then( json => {
-			res.send( json );
-		})
-	})
+
+  fetch( "http://api.xfyun.cn/v1/service/v1/iat" , {
+  	method: "POST",
+  	headers: {
+  		"Content-Type":req.get("Content-Type"),
+  		"X-Appid":req.get("X-Appid"),
+  		"X-CurTime":req.get("X-CurTime"),
+  		"X-Param":req.get("X-Param"),
+  		"X-CheckSum":req.get("X-CheckSum")
+  	},
+  	body: req.body
+  }).then( response => {
+  	response.json().then( json => {
+  		res.send( json );
+  	})
+  })
 }
