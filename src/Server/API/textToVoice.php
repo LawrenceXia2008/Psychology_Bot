beb6b3639f548ff3d1b94aea0f087d2cf7920777
GitAntinus <gantinus@gmail.com>
2018-05-03 22:40:50 +0800

    this is a quick commit

--- src/Server/API/textToVoice.php
+++ src/Server/API/textToVoice.php
@@ -116,7 +116,17 @@ curl_close($ch);
 
 $publicDir = realpath( __DIR__ . "/../../../public/" );
 
+function generateRandomString($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomString;
+}
 
-$file = $g_has_error ? "result.txt" : $publicDir."/result.mp3";
+$file = $g_has_error ? "result.txt" : $publicDir."/result.mp3".generateRandomString( 128 );
+
 file_put_contents($file, $data);
 echo "/result.mp3";
