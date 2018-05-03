a5d725d55092e54a34e2586cb2361962584aa821
GitAntinus <gantinus@gmail.com>
2018-05-03 19:58:10 +0800

    this is a quick commit

--- src/Server/API/textToVoice.js
+++ src/Server/API/textToVoice.js
@@ -3,7 +3,7 @@ var execSync = require('child_process').execSync;
 module.exports = ({ req, res }) => {
   const { text } = req.body;
   console.log( "准备转换:" + text );
-  stdout = execSync( `php textToVoice.php ${text}` );
+  stdout = execSync( `php ./textToVoice.php ${text}` );
   console.log( stdout );
   res.send( stdout );
 }
